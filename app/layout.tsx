e79cc1ec423a45e4b018452b29358f43d10453c3
Taner Solak <56222973+tanersolak@users.noreply.github.com>
2026-07-08 14:58:09 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "James Henderson | Health & Safety Officer",
+  title: "Altan Kaan Kaskan | İş Sağlığı ve Güvenliği Uzmanı",
   description:
     "The first disabled person to work as a Human Health & Safety Officer. Advocate, speaker, and role model.",
 };
